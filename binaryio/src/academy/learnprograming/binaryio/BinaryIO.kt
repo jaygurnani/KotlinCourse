dfb47b8562d9990277a4e9236364b77a1a86a8e4
Jay Gurnani <jay.gurnani@gmail.com>
2021-01-03 19:16:49 +1100

    BinaryIO

---
+++ binaryio/src/academy/learnprograming/binaryio/BinaryIO.kt
@@ -0,0 +1,20 @@
+package academy.learnprograming.binaryio
+
+import java.io.DataInputStream
+import java.io.FileInputStream
+import java.io.EOFException
+fun main (args: Array<String>) {
+
+    val di = DataInputStream(FileInputStream("testfile.bin"))
+    var si: String
+
+    try {
+        while(true) {
+            si = di.readUTF()
+            println(si)
+        }
+    }
+    catch (e: EOFException) {
+
+    }
+}
